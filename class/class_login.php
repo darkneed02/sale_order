229bd253bd2e62810f26fc81e2ae28894096325a
Jaruwat Amnuaysat <66204686+darkneed02@users.noreply.github.com>
2024-05-14 15:43:39 +0700

    deleted file
    ผิดโฟลเดอร์

--- class/class_login.php
+++
@@ -1,6 +0,0 @@
-<?php
-    function check_user_login($conn,$username,$password){
-        $query = "select * from tb_emp where username = '$username' and password = '$password' ";
-        return mysqli_query($conn,$query);
-    }
-?>
--- js/password-eye.js
+++
@@ -1,9 +0,0 @@
-const passwordInput = document.getElementById('password');
-const togglePasswordIcon = document.getElementById('togglePasswordIcon');
-
-togglePasswordIcon.addEventListener('click', function() {
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-    togglePasswordIcon.classList.toggle('fa-eye-slash');
-    togglePasswordIcon.classList.toggle('fa-eye');
-});
